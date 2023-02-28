ec669bf89dc8c6fdf7d7b98171c53a970e7746a7
mugiwararf <ayumi04190703@gmail.com>
2023-02-28 21:21:07 +0900

    ログイン、表示変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-          validates :nickname,      presence: true
-          validates :first_name,    presence: true
-          validates :last_name,     presence: true
-          validates :first_kana,    presence: true        
-          validates :last_kana,     presence: true
-          validates :date,          presence: true
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-
-
+  validates :nickname, presence: true
+  # ひらがな カナ 漢字
+  with_options presence: true do
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :last_name,  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  end
+  # カタカナ
+  validates :first_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/  }
+  validates :last_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/  }
+  validates :birthday,          presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
