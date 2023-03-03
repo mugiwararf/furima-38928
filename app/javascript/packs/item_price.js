7c749cd79392b6606d5c6a950c7da7fc3c8d7886
mugiwararf <ayumi04190703@gmail.com>
2023-03-03 16:34:27 +0900

    JavaScript実装

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -1,5 +1,10 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
-
-});
+  priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   const addTaxDom = document.getElementById("add-tax-price")
+   const proFitDom = document.getElementById("profit")
+   addTaxDom.innerHTML = Math.floor( inputValue * 0.1 )
+   proFitDom.innerHTML = inputValue - addTaxDom.innerHTML
+  })
+})
