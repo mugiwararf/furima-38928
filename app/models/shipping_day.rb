c20cf2e6cd94ddc6c103ec8df3ef96cf682d47b2
mugiwararf <ayumi04190703@gmail.com>
2023-03-10 11:36:09 +0900

    active hush修正

--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,9 +1,9 @@
 class ShippingDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '一1日~2日で発送' },
-    { id: 3, name: '一2日~3日で発送' },
-    { id: 4, name: '一4日~7日で発送' }
+    { id: 2, name: '1日~2日で発送' },
+    { id: 3, name: '2日~3日で発送' },
+    { id: 4, name: '4日~7日で発送' }
 
   ]
 
